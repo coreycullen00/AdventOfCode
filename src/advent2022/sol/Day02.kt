fa0a0e10a8a1bece75030f261c421c8dff410f15
Corey Cullen <coreycullen00@live.ie>
2023-11-30 22:06:46 +0000

    setup + input

---
+++ src/advent2022/sol/Day02.kt
@@ -0,0 +1,19 @@
+package advent2022.sol
+
+import advent2022.Utils.Companion.getInput
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val input = readInput(getInput("Day02"))
+    part1(input).println()
+    part2(input).println()
+}
