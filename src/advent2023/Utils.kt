1b842ada97321069260cee65e044ddaaed1e6e13
Corey <coreycullen00@live.ie>
2024-12-05 18:01:41 +0000

    2024/Day03 (#11)

--- src/advent2023/Utils.kt
+++ src/advent2023/Utils.kt
@@ -3,7 +3,7 @@ package advent2023
 class Utils {
     companion object{
         const val INPUT = "advent2023/input"
-        fun getInput(name: String) = "$INPUT/$name"
-        fun getTestInput(name: String) = "$INPUT/test/$name"
+        internal fun getInput(name: String) = "$INPUT/$name"
+        internal fun getTestInput(name: String) = "$INPUT/test/$name"
     }
 }
