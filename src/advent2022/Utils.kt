d7bed5cc9c234235fb5e38a982ad13ff6eebb21b
Corey Cullen <coreycullen00@live.ie>
2024-12-05 18:01:11 +0000

    2024/Day03

--- src/advent2022/Utils.kt
+++ src/advent2022/Utils.kt
@@ -3,7 +3,7 @@ package advent2022
 class Utils {
     companion object{
         private const val INPUT = "advent2022/input"
-        fun getInput(name: String) = "$INPUT/$name"
-        fun getTestInput(name: String) = "${INPUT}/test/$name"
+        internal fun getInput(name: String) = "$INPUT/$name"
+        internal fun getTestInput(name: String) = "${INPUT}/test/$name"
     }
 }
--- src/advent2024/Utils.kt
+++ src/advent2024/Utils.kt
@@ -3,7 +3,7 @@ package advent2024
 class Utils {
     companion object{
         const val INPUT = "advent2024/input"
-        fun getInput(name: String) = "$INPUT/$name"
-        fun getTestInput(name: String) = "$INPUT/test/$name"
+        internal fun getInput(name: String) = "$INPUT/$name"
+        internal fun getTestInput(name: String) = "$INPUT/test/$name"
     }
 }
